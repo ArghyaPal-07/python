1b90832df1ef60fc2125cf5b6601f1ef2cf411ef
ArghyaPal-07 <arghyapal072005@gmail.com>
2025-03-25 21:56:24 +0530

    new file

--- main2.py
+++
@@ -1,7 +0,0 @@
-f = open("myText.txt", "r")
-s=[]
-for a in f:
-    s.append(a)
-s.sort()
-print (s)
-f.close()
---
+++ oops.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,roll_no,name):
+        self.roll_no = roll_no 
+        self.name = name
+
+    def display(self):
+        print(self.roll_no, self.name)
+
+s0 = Student(0, 'Ap')
+s0. display()
+
+s1 = Student(1, 'Bp')
+s1. display()
