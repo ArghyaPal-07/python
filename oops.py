5b146a8cd7bedfb551e4d88b55051e319e9bd48a
ArghyaPal-07 <arghyapal072005@gmail.com>
2025-03-25 22:04:25 +0530

    oops practice

--- oops.py
+++ oops.py
@@ -1,13 +1,22 @@
 class Student:
-    def __init__(self,roll_no,name):
+    def __init__(self,roll_no,name,total):
         self.roll_no = roll_no 
         self.name = name
+        self.total = total
 
     def display(self):
-        print(self.roll_no, self.name)
+        print(self.roll_no, self.name,self.total)
+    
+    def result(self):
+        if self.total > 120:
+            print('Pass')
+        else:    
+            print('Fail')
 
-s0 = Student(0, 'Ap')
+s0 = Student(0, 'Ap', 500)
 s0. display()
+s0. result()
 
-s1 = Student(1, 'Bp')
+s1 = Student(1, 'Bp', 120)
 s1. display()
+s1. result()
